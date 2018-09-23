6fea057db49803fd240d30ac762410e0707310e6
web-dev-media <info@web-dev-media.de>
2018-09-23 18:56:14 +0200

    Update 06-motd.sh

--- puphpet/files/exec-once/06-motd.sh
+++ puphpet/files/exec-once/06-motd.sh
@@ -1,29 +1,4 @@
-
-	CPUTIME=$(ps -eo pcpu | awk ‘NR>1’ | awk ‘{tot=tot+$1} END {print tot}’)
-	CPUCORES=$(cat /proc/cpuinfo | grep -c processor)
-	UP=$(echo `uptime` | awk ‘{ print $3 ” ” $4 }’)
-	
-	echo ”
-	
-                                            ______              _____ 
-___      _________   ____________  ____________  /________________  /_
-__ | /| / ___  __ \  ___  __ _  / / ___  __ __  __ ___  __ _  _ _  __/
-__ |/ |/ /__  /_/ /  __  /_/ / /_/ /__  /_/ _  / / __  /_/ /  __/ /_  
-____/|__/ _  .___/   _  .___/\__,_/ _  .___//_/ /_/_  .___/\___/\__/  
-          /_/        /_/            /_/            /_/   
-	
-	System Status
-	Updated: `date`
-	
-	– Server Name               = `hostname`
-	– Public IP                 = `dig +short myip.opendns.com @resolver1.opendns.com`
-	– OS Version                = `cat /etc/redhat-release`
-	– Load Averages             = `cat /proc/loadavg`
-	– System Uptime             = `echo $UP`
-	– Platform Data             = `uname -orpi`
-	– CPU Usage (average)       = `echo $CPUTIME / $CPUCORES | bc`%
-	– Memory free (real)        = `free -m | head -n 2 | tail -n 1 | awk {‘print $4’}` Mb
-	– Memory free (cache)       = `free -m | head -n 3 | tail -n 1 | awk {‘print $3’}` Mb
-	– Swap in use               = `free -m | tail -n 1 | awk {‘print $3’}` Mb
-	– Disk Space Used           = `df / | awk ‘{ a = $4 } END { print a }’`
-	" > /etc/motd
+#! /usr/bin/env bash
+sudo ln -s /vagrant/puphpet/files/motd/03-wp-puphpet /etc/update-motd.d/
+sudo ln -s /vagrant/puphpet/files/motd/04-serverinfo /etc/update-motd.d/
+sudo ln -s /vagrant/puphpet/files/motd/05-domain-info /etc/update-motd.d/
