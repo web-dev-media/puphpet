922042bcb673e19d11fc7718f78094eda462ca91
Rene Reimann <info@rene-reimann.de>
2018-09-24 07:38:44 +0200

    update motd handling

--- puphpet/files/exec-once/06-motd.sh
+++ puphpet/files/exec-once/06-motd.sh
@@ -1,4 +1,14 @@
 #! /usr/bin/env bash
-sudo ln -s /vagrant/puphpet/files/motd/03-wp-puphpet /etc/update-motd.d/
-sudo ln -s /vagrant/puphpet/files/motd/04-serverinfo /etc/update-motd.d/
-sudo ln -s /vagrant/puphpet/files/motd/05-domain-info /etc/update-motd.d/
+#sudo cp /vagrant/puphpet/files/motd/* /etc/update-motd.d/
+#sudo ln -s /vagrant/puphpet/files/motd/04-serverinfo /etc/update-motd.d/
+#sudo ln -s /vagrant/puphpet/files/motd/05-domain-info /etc/update-motd.d/
+
+#echo "Disable /var/run/motd.dynamic."
+#sudo systemctl disable motd
+#sudo rm /var/run/motd.dynamic
+
+echo "Copy wp-puphpet motd files to /etc/update-motd.d"
+sudo cp /vagrant/puphpet/files/motd/* /etc/update-motd.d/
+sudo chmod +x /etc/update-motd.d/*
+
+run-parts /etc/update-motd.d > /dev/null
